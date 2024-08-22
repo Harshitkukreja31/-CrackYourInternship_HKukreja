404c1141559ba97ecbf71a3bb40211b32a3cb5ec
Harshitkukreja31 <124774505+Harshitkukreja31@users.noreply.github.com>
2024-08-22 11:54:22 +0530

    Time: 85 ms (19.89%), Space: 34.2 MB (17.63%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> ugly(n,0);
-        ugly[0]=1;
-        int i2=0;
-        int i3=0;
-        int i5=0;
-        int nextMultiple2=ugly[i2]*2;
-        int nextMultiple3=ugly[i3]*3;
-        int nextMultiple5=ugly[i5]*5;
-        int next_ugly_no=ugly[0];
-        for(int i=1;i<n;i++){
-            next_ugly_no=min(nextMultiple2,min(nextMultiple3,nextMultiple5));
-            ugly[i]=next_ugly_no;
-            if(next_ugly_no==nextMultiple2){
-                i2+=1;
-                nextMultiple2=ugly[i2]*2;
-            }
-            if(next_ugly_no==nextMultiple3){
-                i3+=1;
-                nextMultiple3=ugly[i3]*3;
-            }
-            if(next_ugly_no==nextMultiple5){
-                i5+=1;
-                nextMultiple5=ugly[i5]*5;
+        vector<int> primes={2,3,5};
+        priority_queue<long,vector<long>,greater<long>> pq;
+        unordered_set<long> visited;
+        pq.push(1);
+        visited.insert(1);
+        long curr;
+        for(int i=0;i<n;i++){
+            curr=pq.top();
+            pq.pop();
+            for(int prime:primes){
+                long new_ugly=curr*prime;
+                if(visited.find(new_ugly)==visited.end()){
+                    pq.push(new_ugly);
+                    visited.insert(new_ugly);
+                }
             }
         }
-        return next_ugly_no;
-
+        return (int)curr;
     }
 };
